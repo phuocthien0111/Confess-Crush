e5de55de60e0a80a03e798944c259f823478cd15
phuocthien0111 <106002353+phuocthien0111@users.noreply.github.com>
2022-05-21 21:26:53 -0700

    Update config.js

--- config.js
+++ config.js
@@ -1,20 +1,17 @@
 const CONFIG = {
-    titleWeb: "Tiêu đề web",
-    introTitle: 'Tên người yêu bạn',
-    introDesc: `Trái đất vốn lạ thường
-    Mà sao em cứ đi nhầm đường
-    Lạc vào tim anh lẻ loi
-    Đằng sau chữ yêu đây là thương`,
-    btnIntro: '^^HiHi^^',
-    title: 'Phải chăng em đã yêu ngay từ cái nhìn đầu tiên 🥰',
-    desc: 'Phải chăng em đã say ngay từ lúc thấy nụ cười ấy ',
-    btnYes: 'Thích lắm <33',
-    btnNo: 'Không nha :3',
-    question: 'Trên thế giới hơn 7 tỉ người mà sao bạn lại yêu mình <3',
-    btnReply: 'Gửi cho bạn <3',
-    reply: 'Yêu thì yêu mà không yêu thì yêu <33333333',
-    mess: 'Mình biết mà 🥰. Yêu bạn nhiều nhiều 😘😘',
-    messDesc: 'Tối nay 7h, mình qua đón đi chơi nha.',
-    btnAccept: 'Okiiiii lun <3',
-    messLink: 'https://github.com/zukahai/Confess-Crush' //link mess của các bạn. VD: https://www.facebook.com/messages/t/100014188333536
+    titleWeb: "Chúc Mừng Sinh Nhật Em!!",
+    introTitle: 'Phượng Quyên',
+    introDesc: `23-05-2003`,
+    btnIntro: '19-04-2022',
+    title: 'Chúc em ngày càng xinh đẹp giỏi giang và yêu anh nhiều hơn 🥰',
+    desc: 'Hay anh tổ chức tiệc sinh nhật em nha?? ',
+    btnYes: 'Hông chịu đâu',
+    btnNo: 'Dạ vâng',
+    question: 'Mình quen nhau mấy ngày rồi nhỡ',
+    btnReply: 'Gửi',
+    reply: 'Em quên rồi <33333333',
+    mess: 'Anh quên mua quà sinh nhật em rồi',
+    messDesc: 'Nhưng nhớ chờ điện thoại anh shipper nha',
+    btnAccept: 'Okiiiii',
+    messLink: 'https://www.facebook.com/profile.php?id=100075356868613' //link mess của các bạn. VD: https://www.facebook.com/messages/t/100014188333536
 }
